3bb4e2c6def9c2190e6a5e4632a9559b6ebf8732
Evan Mora <evanmorag@gmail.com>
2026-05-09 09:11:11 -0500

    Setup and test of the flatlaf library

---
+++ README.md
@@ -0,0 +1,19 @@
+# Graph Program
+
+## How to run
+1. Compile: 
+'''
+javac -cp "./lib/*" -d bin src/*.java 
+'''
+2. Execute:
+'''
+java -cp "bin:./lib/*" Main
+'''
+3. Script (Compile and Execute the program):
+'''
+bash run.sh
+'''
+NOTE: Run this comands on the root of the proyect
+
+## Dependencies
+- flatlaf 3.7.1
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            System.out.println("No se pudo cargar FlatLaf");
+        }
+
+        JFrame frame = new JFrame("Prueba FlatLaf");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Hello world");
+        frame.add(button);
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
